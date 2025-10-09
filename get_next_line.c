f14822240057b1665806425787f35f5f26adcd1a
Theo Bouverat <thbouver@c1r12s6.42lausanne.ch>
2025-10-09 16:27:41 +0200

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,34 +6,11 @@
 /*   By: thbouver <thbouver@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 11:24:48 by thbouver          #+#    #+#             */
-/*   Updated: 2025/10/09 15:11:32 by thbouver         ###   ########.fr       */
+/*   Updated: 2025/10/09 16:27:17 by thbouver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-void	get_line_from_cache(char **cache, char **dest)
-{
-	char	*cache_line;
-	char	*new_cache;
-	int		cache_index;
-	int		index;
-
-	cache_index = 0;
-	index = 0;
-	new_cache = ft_calloc(ft_strlen(*cache) + 1, sizeof(char));
-	cache_line = ft_calloc(ft_strlen(*cache) + 1, sizeof(char));
-	while ((*cache)[index] && (*cache)[index] != '\n')
-		cache_line[cache_index ++] = (*cache)[index ++];
-	if ((*cache)[index] == '\n')
-		cache_line[cache_index] = (*cache)[index ++];
-	cache_index = 0;
-	while ((*cache)[index])
-		new_cache[cache_index ++] = (*cache)[index ++];
-	free (*cache);
-	*cache = new_cache;
-	*dest = cache_line;
-}
 
 void	trim_and_save(char *line, char *cache)
 {
@@ -78,32 +55,57 @@ int	ft_init(char **buffer, char **cache, char **line, int fd)
 	return (1);
 }
 
+int	get_line_from_cache(char **cache, char **dest, int complete)
+{
+	char	*cache_line;
+	char	*new_cache;
+	int		cache_index;
+	int		index;
+
+	cache_index = 0;
+	index = 0;
+	if ((complete == 1 && !ft_strchr(*cache, '\n'))
+		|| (complete == 0 && !(ft_strlen(*cache) > 0)))
+		return (0);
+	new_cache = ft_calloc(ft_strlen(*cache) + 1, sizeof(char));
+	cache_line = ft_calloc(ft_strlen(*cache) + 1, sizeof(char));
+	if (!new_cache || !cache_line)
+		return (0);
+	while ((*cache)[index] && (*cache)[index] != '\n')
+		cache_line[cache_index ++] = (*cache)[index ++];
+	if ((*cache)[index] == '\n')
+		cache_line[cache_index] = (*cache)[index ++];
+	cache_index = 0;
+	while ((*cache)[index])
+		new_cache[cache_index ++] = (*cache)[index ++];
+	free (*cache);
+	*cache = new_cache;
+	*dest = cache_line;
+	return (1);
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*cache;
 	char		*buffer;
 	char		*line;
-	int			count;
+	int			info;
 
 	if (!ft_init(&buffer, &cache, &line, fd))
 		return (NULL);
-	if (ft_strchr(cache, '\n'))
-		get_line_from_cache(&cache, &line);
-	else
+	if (get_line_from_cache(&cache, &line, 1))
+		return (ft_exit(&cache, buffer, 0, line));
+	info = read(fd, buffer, BUFFER_SIZE);
+	if ((info == 0 && ft_strlen(cache) == 0))
+		return (ft_exit(&cache, buffer, 1, NULL));
+	get_line_from_cache(&cache, &line, 0);
+	line = ft_realloc(line, buffer);
+	while (info != 0 && !ft_strchr(buffer, '\n'))
 	{
-		count = read(fd, buffer, BUFFER_SIZE);
-		if ((count == 0 && ft_strlen(cache) == 0))
-			return (ft_exit(&cache, buffer, 1, NULL));
-		if (ft_strlen(cache) > 0)
-			get_line_from_cache(&cache, &line);
+		ft_bzero(buffer, BUFFER_SIZE + 1);
+		info = read(fd, buffer, BUFFER_SIZE);
 		line = ft_realloc(line, buffer);
-		while (count != 0 && !ft_strchr(buffer, '\n'))
-		{
-			ft_bzero(buffer, BUFFER_SIZE + 1);
-			count = read(fd, buffer, BUFFER_SIZE);
-			line = ft_realloc(line, buffer);
-		}
-		trim_and_save(line, cache);
 	}
+	trim_and_save(line, cache);
 	return (ft_exit(&cache, buffer, 0, line));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: thbouver <thbouver@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 11:24:40 by thbouver          #+#    #+#             */
-/*   Updated: 2025/10/09 15:12:37 by thbouver         ###   ########.fr       */
+/*   Updated: 2025/10/09 16:22:31 by thbouver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,6 @@ char	*ft_realloc(char *dest, char *src);
 void	*ft_calloc(size_t count, size_t size);
 void	ft_bzero(void *pointer, size_t n);
 int		ft_strlen(const char *str);
-int     ft_strchr(char *str, char c);
+int		ft_strchr(char *str, char c);
 
 #endif
